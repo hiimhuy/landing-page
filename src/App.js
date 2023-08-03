d66d8ec0de9eda39d1a376abd680603c0eeaa778
hiimhuy <conghuy123zzz@gmail.com>
2023-08-03 16:00:50 +0700

    upload- err darkmode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Content from './components/Content/Content';
-import Navbar from './components/Navbar/Navbar';
 import Slogan from './components/Slogan/Slogan';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+    const [windowSize, setWindowSize] = useState({
+        width: undefined,
+        height: undefined,
+    });
+    const [isMobile, setMobile] = useState(false);
+
+    useEffect(() => {
+        const handleSize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                heigh: window.innerHeight,
+            });
+        };
+        window.addEventListener('resize', handleSize);
+        handleSize();
+        return () => window.removeEventListener('resize', handleSize);
+    }, []);
+
+    useEffect(() => {
+        if (windowSize.width < 500) {
+            setMobile(true);
+        } else {
+            setMobile(false);
+        }
+    }, [windowSize]);
     return (
-        <div className="font-sora overflow-y-auto h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to-purple-700 text-white">
-            <Navbar />
+        <div className="font-sora overflow-y-auto overflow-x-hidden h-screen px-4 py-8 bg-gradient-to-b dark:from-purple-900 dark:to-purple-700 from-white to-pink-500  dark:text-white text-black sm:px-20">
+            <Navbar isMobile={isMobile} />
             <Slogan />
             <Content />
         </div>
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,42 +1,48 @@
-import {FaUser} from 'react-icons/fa'
-import { BsFillCreditCardFill } from 'react-icons/bs';
-import checkIconMobile from '../../assets/mobile/checkpoint.svg'
-
+import { FaUser } from "react-icons/fa";
+import { useSpring, animated } from "react-spring";
+import checkIconMobile from "../../assets/mobile/checkpoint.svg";
+import { BsFillCreditCardFill } from "react-icons/bs";
 const Content = () => {
-    return ( 
-        <section className="">
-            <div className="ml-[70px] mr-[54px] flex flex-col items-center bg-gradient-to-b shadow-md from-white/40 to-transparent rounded-[24px]">
-                <div className="mt-16 gap-8 flex items-start">
-                    <FaUser size={'22px'} className='mt-[12px]'/>
-                    <p className='text-26'>
-                        10245
-                        <p className='text-13 font-semibold mb-[36px]'>Customers</p>
-                    </p>
-                </div>
-                <div className="mb-16 gap-8 flex items-start">
-                    <BsFillCreditCardFill size={'22px'} className='mt-[12px]'/>
-                    <p className="text-26">
-                        12045
-                        <p className='text-13 font-semibold'>Cards Issued</p>
-                    </p>
-                </div>
-            </div>
-            <div className="text-13 flex flex-col w-[100%] mt-[90px] font-semibold">
-                <p className='check-content'>
-                    <img src={checkIconMobile} alt="" className="" />
-                    <p>Card reports sent to your phone every weeks</p>
-                </p>
-                <p className='check-content'>
-                    <img src={checkIconMobile} alt="" className="" />
-                    <p>No external fees</p>
-                </p>
-                <p className='check-content'>
-                    <img src={checkIconMobile} alt="" className="" />
-                    <p>Set spending limits and restrictions</p>
-                </p>
-            </div>
-        </section>
-     );
-}
- 
+  const customer = useSpring({ customers: 10245, from: { customers: 0 } });
+  const card = useSpring({ cards: 12045, from: { cards: 0 } });
+  return (
+    <section className="md:flex md:flex-row">
+      <div className="ml-[70px] mr-[54px] text-bold flex flex-col items-center bg-gradient-to-b shadow-md from-white/40 rounded-[24px] to-transparent md:w-[20%]">
+        <div className="mt-16 gap-8 flex items-start">
+          <FaUser size={"22px"} className="mt-[12px]" />
+          <div className="text-26">
+            <animated.div>
+              {customer.customers.to((val) => Math.floor(val))}
+            </animated.div>
+            <p className="text-13 font-semibold mb-[36px] md:text-16">Customers</p>
+          </div>
+        </div>
+        <div className="gap-8 flex items-start mb-16">
+          <BsFillCreditCardFill size={"22px"} className="mt-[12px]" />
+          <div className="text-26">
+            <animated.div>
+              {card.cards.to((val) => Math.floor(val))}
+            </animated.div>
+            <p className="text-13 font-semibold md:text-16"> Cards Issued </p>
+          </div>
+        </div>
+      </div>
+      <div className="text-13 flex flex-col w-[100%] mt-[90px] font-semibold md:w-[30%] md:mt-0 md:text-16 md:ml-[20%]">
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p> Card reports sent to your phone every weeks </p>
+        </div>
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p>No external fees </p>
+        </div>
+        <div className="check-content">
+          <img src={checkIconMobile} alt="" />
+          <p>Set spending limits and restrictions</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default Content;
